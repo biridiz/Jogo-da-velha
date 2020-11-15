6d4d9955496a57c5453470a0bae80d7a878f44c3
Luiz Dendena <luiz.dendena@gmail.com>
2020-11-15 15:07:07 -0300

    Jogo da velha :)

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../styles/index.css';
+import Board from '../pages/index';
+
+class Index extends Component {
+    state = {};
+    render() {
+      return <Board />;
+    }
+  }
+  
+  export default Index;
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,104 @@
-import { useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function Home() {
-    return (
-        <div>
-            <h4>Home 4</h4>
-            <Contador />
-            <div>Teste</div>
-        </div>
-    )
-}
 
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
+class Board extends React.Component {
+    /**
+     *   Nota:
+     *   Em classes JavaScript , você precisa sempre chamar 
+     *   super ao definir o construtor de uma subclasse.
+     *   Todas as classes de componentes React que possuem um 
+     *   constructor devem começar com uma super(props) chamada.
+    **/
+    constructor(props) {
+       super(props);
+       this.state = {
+           squares: Array(9).fill(null),
+           xIsNext: true,
+       };
     }
 
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({ 
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            const status = 'Next player: ' + (this.statusIsNext ? 'X' : 'O');
+        }
+
+        return (
+            <div>
+                <div className="status">{ status }</div>
+                <div className="board-row">
+                    { this.renderSquare(0) }
+                    { this.renderSquare(1) }
+                    { this.renderSquare(2) }
+                </div>
+                <div className="board-row">
+                    { this.renderSquare(3) }
+                    { this.renderSquare(4) }
+                    { this.renderSquare(5) }
+                </div>
+                <div className="board-row">
+                    { this.renderSquare(6) }
+                    { this.renderSquare(7) }
+                    { this.renderSquare(8) }
+                </div>
+            </div>
+        );
+    }
 }
 
-export default Home
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={ props.onClick }
+        >
+            { props.value }
+        </button>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+    for (let i = 0; i< lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return null
+}
+
+export default Board
